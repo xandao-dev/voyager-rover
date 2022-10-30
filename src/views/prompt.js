a1c658fd131f2b4cf0aee1ffbe0e8dac02bf2fe5
Alexandre Calil <alexandrecalilmf@gmail.com>
2022-10-30 00:19:57 -0300

    chore: simplify prompt and tests

--- src/views/prompt.js
+++ src/views/prompt.js
@@ -1,17 +1,10 @@
 const readline = require('readline');
-const { ViewError } = require('../errors/view-error.js');
 
-function createPrompt(configs = {}) {
-  const readlineInterface =
-    configs.readlineInterface ||
-    readline.createInterface({
-      input: process.stdin,
-      output: process.stdout,
-    });
-
-  if (!readlineInterface?.question || !readlineInterface?.close) {
-    throw new ViewError({ message: 'Invalid readline interface', view: 'prompt' });
-  }
+function createPrompt() {
+  const readlineInterface = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
 
   const ask = (question) => new Promise((resolve) => readlineInterface.question(question, (answer) => resolve(answer)));
   const close = () => readlineInterface.close();
--- tests/views/prompt.test.js
+++ tests/views/prompt.test.js
@@ -1,27 +1,22 @@
 const { createPrompt } = require('../../src/views/prompt.js');
 
+jest.mock('readline', () => ({
+  createInterface: jest.fn(() => ({
+    question: jest.fn((_, callback) => callback('Voyager Rover')),
+    close: jest.fn().mockReturnValue('close'),
+  })),
+}));
+
+let prompt;
+beforeEach(() => {
+  prompt = createPrompt();
+});
+afterEach(() => {
+  jest.clearAllMocks();
+});
+
 describe('createPrompt', () => {
   describe('happy path', () => {
-    const readlineInterfaceMock = {
-      question: jest.fn((_, callback) => callback('Voyager Rover')),
-      close: jest.fn(),
-    };
-
-    let prompt;
-    beforeEach(() => {
-      prompt = createPrompt({ readlineInterface: readlineInterfaceMock });
-    });
-    afterEach(() => {
-      jest.clearAllMocks();
-    });
-
-    it('should not throw with the original readline interface', () => {
-      expect(() => {
-        const prompt = createPrompt();
-        prompt.close();
-      }).not.toThrow();
-    });
-
     it('should return an object with ask and close methods', () => {
       expect(prompt).toHaveProperty('ask');
       expect(prompt).toHaveProperty('close');
@@ -31,46 +26,20 @@ describe('createPrompt', () => {
 
     describe('ask', () => {
       it('should return a promise', () => {
-        expect.assertions(1);
         const result = prompt.ask('What is your name? ');
         expect(result).toBeInstanceOf(Promise);
       });
 
-      it('should call readlineInterface.question with the question "What is your name? "', async () => {
-        expect.assertions(1);
-        await prompt.ask('What is your name? ');
-        expect(readlineInterfaceMock.question).toHaveBeenCalledWith('What is your name? ', expect.any(Function));
-      });
-
       it('should resolve the promise with the answer "Voyager Rover"', async () => {
-        expect.assertions(1);
         const result = await prompt.ask('What is your name? ');
         expect(result).toBe('Voyager Rover');
       });
 
-      it('should call readlineInterface.question once', async () => {
-        expect.assertions(1);
-        await prompt.ask('What is your name? ');
-        expect(readlineInterfaceMock.question).toHaveBeenCalledTimes(1);
+      it('should not throw when call close method', () => {
+        expect(() => {
+          prompt.close();
+        }).not.toThrow();
       });
-    });
-
-    describe('close', () => {
-      it('should call readlineInterface.close once', () => {
-        prompt.close();
-        expect(readlineInterfaceMock.close).toHaveBeenCalled();
-      });
-    });
-  });
-
-  describe('unhappy path', () => {
-    const readlineInterfaceMock = {
-      question: undefined,
-      close: undefined,
-    };
-
-    it('should throw an error if readlineInterface.question or readlineInterface.close is not a function', () => {
-      expect(() => createPrompt({ readlineInterface: readlineInterfaceMock })).toThrow();
     });
   });
 });
