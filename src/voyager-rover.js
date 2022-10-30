cf3a76ecc7f9b82fb3143e1a7fe65eab790014d7
Alexandre Calil <alexandrecalilmf@gmail.com>
2022-10-30 00:58:02 -0300

    docs: add a simple docs at startup

--- src/voyager-rover.js
+++ src/voyager-rover.js
@@ -1,6 +1,18 @@
 const { createSimulatorController } = require('./controllers/simulator-controller.js');
 (async () => {
   try {
+    console.log(
+      'Welcome to Voyager Rover\n',
+      'A fictional calculator of final coordinates of a squadron of robotic rovers landed by NASA on a plateau on Mars.\n\n',
+      'To start the simulator, follow the instructions below:\n',
+      '1. Enter the plateau size, width and height. (e.g. 5 5)\n',
+      '2. Enter the rover landing position, x and y coordinates and the direction it is facing (N, S, E, W). (e.g. 1 2 N)\n',
+      '3. Enter the movement instructions for the rover L for left, R for right and M for move. (e.g. LMLMLMLMMRM)\n\n',
+      'Repeat steps 2 and 3 for as many rovers as you want\n\n',
+      'To exit the simulator, press Ctrl + C\n',
+      '-------------------------------------------------------------\n'
+    );
+
     const simulatorController = createSimulatorController();
     await simulatorController.run();
   } catch (error) {
